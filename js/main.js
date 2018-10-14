696884e324ba77b9f3d914808dc144cf0884ca02
Olha Maslova <my_love@Serhiis-MacBook-Pro.local>
2018-10-13 21:57:48 -0400

    Add service worker

--- js/main.js
+++ js/main.js
@@ -5,6 +5,20 @@ var newMap
 var markers = []
 
 /**
+ *Register service worker 
+  */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then(reg => {
+      console.log("Service worker registration successful: " + reg.scope);
+    })
+    .catch(error => {
+      console.log ("Registration failed: " + error);
+    });
+}
+
+/**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
---
+++ sw.js
@@ -0,0 +1,38 @@
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open('restaurant-review-001').then(function(cache) {
+			console.log('SW is caching');
+			return cache.addAll([
+				'/',
+				'index.html',
+				'restaurant.html',
+				'js/main.js',
+				'js/restaurant_info.js',
+				'js/dbhelper.js',
+				'css/styles.css',
+				'css/responsive.css',
+				'img/1.jpg',
+				'img/2.jpg',
+				'img/3.jpg',
+				'img/4.jpg',
+				'img/5.jpg',
+				'img/6.jpg',
+				'img/7.jpg',
+				'img/8.jpg',
+				'img/9.jpg',
+				'img/10.jpg',
+				'img/na.jpg'
+			]);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			if (response) return response;
+			return fetch(event.request)
+		})
+	);
+});
+
